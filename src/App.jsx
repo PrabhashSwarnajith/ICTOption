77e35a242482b83553da259b4932c055782c19c2
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-07-18 10:59:49 +0530

    Update routing method and configure base path in Vite for improved deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header, Footer } from './components';
 import { Home, Services, Products, AboutUs, Blog, ContactUs } from './pages';
 import {
