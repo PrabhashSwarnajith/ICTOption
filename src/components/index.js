38ae1f8c838398f5f66a03835310b8f018779b40
N1331714 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-12-20 15:02:55 +0530

    feat: Add Back to Top button component with scroll functionality and animations; enhance Tailwind CSS configurations with new animations

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,10 @@
 export { default as Footer } from './common/Footer';
 export { default as Header } from './common/Header';
 export { default as Hero } from './common/Hero';
+export { default as HeroEnhanced } from './common/HeroEnhanced';
+export { default as BackToTop } from './BackToTop';
+export { default as PageLoadingSkeletons } from './PageLoadingSkeletons';
+export { default as PageTransition } from './PageTransition';
 export { default as ProductCard } from './cards/ProductCard';
 export { default as ServiceCard } from './cards/ServiceCard';
 export { default as TestimonialCard } from './cards/TestimonialCard';
